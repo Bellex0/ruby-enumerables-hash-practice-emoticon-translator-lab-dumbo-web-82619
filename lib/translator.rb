9bb00c36bded6fae5bbbec19ca8e610bc45ff048
Belle P <bellepx3@gmail.com>
2019-07-14 01:11:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,4 @@
-require "yaml"
+require 'yaml'
 
 def load_library(file_path)
   library = {"get_meaning" => {}, "get_emoticon" => {} }
@@ -19,14 +19,13 @@ def get_japanese_emoticon(file_path, emoticon)
     "Sorry, that emoticon was not found"
   end
 end
-  
+
 def get_english_meaning(file_path, emoticon)
-    library = load_library(file_path)
+  library = load_library(file_path)
   result = library["get_meaning"][emoticon]
   if result
     result
   else
     "Sorry, that emoticon was not found"
   end
-end
 end
